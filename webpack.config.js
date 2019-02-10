eab7bdc9e44ea4df4e7a7fd7e23e1af975808ddf
Piyush Srivastava <srivastp@gmail.com>
2019-02-10 00:15:40 -0700

    1-Add ES lint support

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
         }
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
         test: /\.html$/,
         use: [
           {
